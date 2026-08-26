13ac7579f833172973cbe888243ac1911e7fb24b
Kimi Admin <kimi@cp.sy3.ru>
2026-08-26 17:19:47 +0300

    Route all booking CTAs to Calendly

--- wordpress/wp-content/themes/neurocoaching/front-page.php
+++ wordpress/wp-content/themes/neurocoaching/front-page.php
@@ -2,7 +2,7 @@
 /** About page reconstructed from the original 1320 px and 320 px PSD artboards. @package Neurocoaching */
 get_header();
 neurocoaching_header( 'about' );
-$booking = neurocoaching_contact_url();
+$booking = neurocoaching_booking_url();
 $instagram = neurocoaching_mod( 'instagram_url', 'https://www.instagram.com/belka80' );
 $images  = get_template_directory_uri() . '/assets/images/';
 $credentials = array(
